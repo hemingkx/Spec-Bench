e4677d9d7ffc2f0d14229750b7acebdab4915d9d
hemingkx <hemingkx@gmail.com>
2024-03-13 11:29:50 +0800

    eagle updated

--- model/eagle/utils_alpha.py
+++ model/eagle/utils_alpha.py
@@ -1,9 +1,6 @@
 import copy
 import random
 
-# typing 
-from typing import List, Tuple
-import time
 import torch
 
 # TODO
@@ -21,38 +18,19 @@ from transformers.generation.logits_process import (
 )
 
 
-def timer(func):
-    def wrapper(*args, **kwargs):
-        torch.cuda.synchronize()
-        start = time.perf_counter()
-
-        result = func(*args, **kwargs)
-
-        torch.cuda.synchronize()
-        elapsed = time.perf_counter() - start
-        print(f'{func.__name__} took {elapsed} seconds')
-        return result
-
-    return wrapper
-
-
 def prepare_logits_processor(
-        temperature: float = 0.0,
-        repetition_penalty: float = 0.0,
-        top_p: float = 0.0,
-        top_k: int = 0
+        temperature=0.0, repetition_penalty=0.0, top_p=0.0, top_k=0
 ) -> LogitsProcessorList:
     processor_list = LogitsProcessorList()
-    if temperature > 1e-5:
-        if temperature >= 1e-5 and temperature != 1.0:
-            processor_list.append(TemperatureLogitsWarper(temperature))
-        if repetition_penalty > 1.0:
-            processor_list.append(RepetitionPenaltyLogitsProcessor(repetition_penalty))
-        if 1e-8 <= top_p < 1.0:
-            processor_list.append(TopPLogitsWarper(top_p))
-        if top_k > 0:
-            processor_list.append(TopKLogitsWarper(top_k))
-        return processor_list
+    if temperature >= 1e-5 and temperature != 1.0:
+        processor_list.append(TemperatureLogitsWarper(temperature))
+    if repetition_penalty > 1.0:
+        processor_list.append(RepetitionPenaltyLogitsProcessor(repetition_penalty))
+    if 1e-8 <= top_p < 1.0:
+        processor_list.append(TopPLogitsWarper(top_p))
+    if top_k > 0:
+        processor_list.append(TopKLogitsWarper(top_k))
+    return processor_list
 
 
 # test_processor = prepare_logits_processor(
@@ -60,7 +38,7 @@ def prepare_logits_processor(
 #     )
 
 
-def pad_path(path: List[int], length: int, pad_value: int = -2) -> List[int]:
+def pad_path(path, length, pad_value=-2):
     """
     Pad the given path list with a specific value up to a specified length.
 
@@ -171,19 +149,6 @@ def generate_tree_buffers(tree_choices, device="cuda"):
     retrieve_indices = torch.cat([torch.zeros((retrieve_indices.shape[0], 1), dtype=torch.long), retrieve_indices],
                                  dim=1)
 
-    maxitem = retrieve_indices.max().item() + 5
-
-    def custom_sort(lst):
-        # sort_keys=[len(list)]
-        sort_keys = []
-        for i in range(len(lst)):
-            sort_keys.append(lst[i] if lst[i] >= 0 else maxitem)
-        return sort_keys
-
-    retrieve_indices = retrieve_indices.tolist()
-    retrieve_indices = sorted(retrieve_indices, key=custom_sort)
-    retrieve_indices = torch.tensor(retrieve_indices, dtype=torch.long)
-
     p_indices = torch.tensor(p_indices)
     p_indices_new = p_indices[retrieve_indices]
     p_indices_new = p_indices_new.tolist()
@@ -242,7 +207,7 @@ def reset_tree_mode(
     model.base_model.model.tree_mode = None
 
 
-def reset_past_key_values(passed_key_values: List[torch.Tensor]) -> List[torch.Tensor]:
+def reset_past_key_values(passed_key_values):
     """
     Resets the current lengths in the passed key-values to zero.
 
@@ -274,7 +239,7 @@ def generate_candidates(tree_logits, tree_indices, retrieve_indices, sample_toke
     tree_candidates = candidates[tree_indices]
 
     tree_candidates_ext = torch.cat(
-        [tree_candidates, torch.zeros((1), dtype=torch.long, device=tree_candidates.device) - 1], dim=0)
+        [tree_candidates, torch.zeros((1), dtype=torch.long, device=tree_candidates.device)], dim=0)
 
     cart_candidates = tree_candidates_ext[retrieve_indices]
 
@@ -318,15 +283,8 @@ def tree_decoding(
 
 
 def evaluate_posterior(
-        logits: torch.Tensor,
-        candidates: torch.Tensor,
-        logits_processor,
-        cart_candidates_prob,
-        op,
-        p_indices,
-        tree_candidates,
-        b_indices
-) -> Tuple[torch.Tensor, int]:
+        logits, candidates, logits_processor, cart_candidates_prob, alpha, alpha_num,op, p_indices, tree_candidates, b_indices
+):
     """
     Evaluate the posterior probabilities of the candidates based on the provided logits and choose the best candidate.
 
@@ -358,6 +316,15 @@ def evaluate_posterior(
             best_candidate = torch.tensor(0, dtype=torch.long, device=candidates.device)
         else:
             best_candidate = torch.argmax(candidates_accept_length).to(torch.long)
+
+        choice_cand = candidates[best_candidate]
+        max_len = torch.count_nonzero(choice_cand)
+        for cand_id in range(len(alpha)):
+            if cand_id < accept_length:
+                alpha[cand_id] += 1
+            if cand_id <= accept_length and cand_id < max_len:
+                alpha_num[cand_id] += 1
+
         return best_candidate, accept_length, logits[best_candidate, accept_length]
 
     else:
@@ -365,25 +332,24 @@ def evaluate_posterior(
         accept_length = 1
         accept_cand = candidates[0][:1]
         best_candidate = 0
+        # breakflag=False
         for i in range(1, candidates.shape[1]):
-            if i != accept_length:
-                break
-            adjustflag = False
             is_eq = (candidates[:, :accept_length] == accept_cand).all(dim=1)
+            if i != accept_length:
+                # breakflag=True
+                break
             fi = torch.nonzero(is_eq, as_tuple=True)[0][0]
             gt_logits = logits[fi, i - 1][None]
             gt_logits = logits_processor(None, gt_logits)[0]
             gtp = torch.softmax(gt_logits, dim=0)
-            candidates_set = []
+            adjustflag = False
             for j in range(candidates.shape[0]):
                 if is_eq[j]:
-                    x = candidates[j, i]
-                    xi = x.item()
-                    if xi in candidates_set or xi == -1:
-                        continue
-                    candidates_set.append(xi)
                     r = random.random()
-                    px = gtp[xi]
+                    x = candidates[j, i]
+                    if x == 0:
+                        continue
+                    px = gtp[x]
                     qx = cart_candidates_prob[j, i]
                     if qx <= 0:
                         continue
@@ -399,17 +365,31 @@ def evaluate_posterior(
                         if len(b) > 0:
                             mask = tree_candidates[0][b]
                             q[mask] = 0
-                            q = q / q.sum()
+                            q = q / (q.sum()+1e-5)
                         gtp = gtp - q
                         gtp[gtp < 0] = 0
                         gtp = gtp / gtp.sum()
+                        # has_nan = torch.isnan(gtp).any()
+                        # if has_nan:
+                        #     print(1)
                         adjustflag = True
-        if adjustflag and accept_length != candidates.shape[1]:
+        if adjustflag:
             sample_p = gtp
         else:
             gt_logits = logits[best_candidate, accept_length - 1]
             sample_p = torch.softmax(gt_logits, dim=0)
-        return torch.tensor(best_candidate), accept_length - 1, sample_p
+
+        accept_length = accept_length - 1
+
+        choice_cand = candidates[best_candidate]
+        max_len = torch.count_nonzero(choice_cand)
+        for cand_id in range(len(alpha)):
+            if cand_id < accept_length:
+                alpha[cand_id] += 1
+            if cand_id <= accept_length and cand_id < max_len:
+                alpha_num[cand_id] += 1
+
+        return torch.tensor(best_candidate), accept_length, sample_p
 
 
 @torch.no_grad()
@@ -462,7 +442,7 @@ def update_inference_inputs(
     else:
         token = torch.argmax(prob)
         token = token[None, None]
-    # hidden_state = torch.cat((hidden_state, accept_hidden_state_new), dim=1)
+    #hidden_state = torch.cat((hidden_state, accept_hidden_state_new), dim=1)
     tree_logits = model.ea_layer.topK_genrate(accept_hidden_state_new,
                                               input_ids=torch.cat((input_ids, token.to(input_ids.device)), dim=1),
                                               head=model.base_model.lm_head, logits_processor=logits_processor)
