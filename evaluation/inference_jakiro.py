445ef1de87ca11348327acee8b10b845aa32e1f1
Hhaiduo <huanghd@stu.xjtu.edu.cn>
2025-05-21 22:37:08 +0800

    add jakiro-star and jakiro inference code

---
+++ evaluation/inference_jakiro.py
@@ -0,0 +1,297 @@
+"""Generate answers with local models.
+
+Usage:
+python3 gen_model_answer.py --model-path lmsys/fastchat-t5-3b-v1.0 --model-id fastchat-t5-3b-v1.0
+"""
+import argparse
+import json
+import os,sys
+script_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(script_dir)
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import time
+import shortuuid
+from fastchat.llm_judge.common import load_questions
+from fastchat.model import get_conversation_template
+from tqdm import tqdm
+
+from pathlib import Path
+# support running without installing as a package
+wd = Path(__file__).parent.parent.resolve()
+sys.path.append(str(wd))
+from model.jakiro.ea_model import EaModel
+from model.jakiro.utils import *
+
+
+def run_eval(
+        base_model_path,
+        ea_model_path,
+        model_id,
+        question_file,
+        question_begin,
+        question_end,
+        answer_file,
+        num_choices,
+        num_gpus_per_model,
+        num_gpus_total,
+        temperature,
+        args
+        ):
+    questions = load_questions(question_file, question_begin, question_end)
+    # random shuffle the questions to balance the loading
+    # random.shuffle(questions)
+    # shuffled_ids = [q["question_id"] for q in questions]
+    # with open(f"data/{args.bench_name}/model_ids/{args.model_id}.shuffled_ids", "w") as fout:
+    #     json.dump(shuffled_ids, fout)
+
+    # Split the question file into `num_gpus` files
+    assert num_gpus_total % num_gpus_per_model == 0
+    use_ray = num_gpus_total // num_gpus_per_model > 1
+
+    if use_ray:
+        get_answers_func = ray.remote(num_gpus=num_gpus_per_model)(get_model_answers).remote
+    else:
+        get_answers_func = get_model_answers
+
+    chunk_size = len(questions) // (num_gpus_total // num_gpus_per_model)
+    ans_handles = []
+    for i in range(0, len(questions), chunk_size):
+        ans_handles.append(
+            get_answers_func(
+                base_model_path,
+                ea_model_path,
+                model_id,
+                questions[i: i + chunk_size],
+                answer_file,
+                num_choices,
+                temperature,
+                args
+            )
+        )
+
+    if use_ray:
+        ray.get(ans_handles)
+
+
+@torch.inference_mode()
+def get_model_answers(
+        base_model_path,
+        ea_model_path,
+        model_id,
+        questions,
+        answer_file,
+        num_choices,
+        temperature,
+        args
+        ):
+    # temperature = 0.0
+
+    model = EaModel.from_pretrained(
+        base_model_path=base_model_path,
+        ea_model_path=ea_model_path,
+        total_token=args.total_token,
+        depth=args.depth,
+        top_k=args.top_k,
+        torch_dtype=torch.float16,
+        low_cpu_mem_usage=True,
+        # load_in_8bit=True,
+        device_map="auto"
+    )
+
+    tokenizer = model.get_tokenizer()
+    model.eval()
+    print('Check model training state:', model.training)
+
+    cuda_visible_devices = os.environ.get('CUDA_VISIBLE_DEVICES')
+    print('CUDA VISIBLE DEVICES:', cuda_visible_devices)
+
+    question = questions[0]
+
+    # warmup
+    for _ in range(3):
+        torch.manual_seed(0)
+        conv = get_conversation_template("vicuna")
+        turns = []
+        idxs = []
+        new_tokens = []
+        wall_time = []
+        for j in range(len(question["turns"])):
+            qs = question["turns"][j]
+            conv.append_message(conv.roles[0], qs)
+            conv.append_message(conv.roles[1], None)
+            prompt = conv.get_prompt()
+            input_ids = tokenizer([prompt]).input_ids
+
+            # try:
+            torch.cuda.synchronize()
+            start_time = time.time()
+            output_ids, new_token, idx = model.eagenerate(
+                torch.as_tensor(input_ids).cuda(), 
+                temperature=temperature, 
+                log=True
+                )
+            torch.cuda.synchronize()
+            total_time = time.time() - start_time
+            output_ids = output_ids[0][len(input_ids[0]):]
+            # be consistent with the template's stop_token_ids
+            if conv.stop_token_ids:
+                stop_token_ids_index = [i for i, id in enumerate(output_ids) if id in conv.stop_token_ids]
+                if len(stop_token_ids_index) > 0:
+                    output_ids = output_ids[: stop_token_ids_index[0]]
+
+            output = tokenizer.decode(output_ids, spaces_between_special_tokens=False,)
+            conv.stop_str = "</s>"
+            if conv.stop_str and output.find(conv.stop_str) > 0:
+                output = output[: output.find(conv.stop_str)]
+            for special_token in tokenizer.special_tokens_map.values():
+                if isinstance(special_token, list):
+                    for special_tok in special_token:
+                        output = output.replace(special_tok, "")
+                else:
+                    output = output.replace(special_token, "")
+            output = output.strip()
+
+            if conv.name == "xgen" and output.startswith("Assistant:"):
+                output = output.replace("Assistant:", "", 1).strip()
+
+            turns.append(output)
+            idxs.append(int(idx))
+            new_tokens.append(int(new_token))
+            wall_time.append(total_time)
+            conv.messages[-1][-1] = output
+    print('Warmup done')
+
+    # questions=questions[6:]
+    for question in tqdm(questions):
+
+        choices = []
+        for i in range(num_choices):
+            torch.manual_seed(i)
+            conv = get_conversation_template("vicuna")
+            turns = []
+            idxs = []
+            new_tokens = []
+            wall_time = []
+            for j in range(len(question["turns"])):
+                qs = question["turns"][j]
+                conv.append_message(conv.roles[0], qs)
+                conv.append_message(conv.roles[1], None)
+                prompt = conv.get_prompt()
+                input_ids = tokenizer([prompt]).input_ids
+
+                torch.cuda.synchronize()
+                start_time = time.time()
+                output_ids, new_token, idx = model.eagenerate(
+                    torch.as_tensor(input_ids).cuda(),
+                    temperature=temperature,
+                    log=True
+                    )
+                torch.cuda.synchronize()
+                total_time = time.time() - start_time
+                output_ids = output_ids[0][len(input_ids[0]):]
+
+                if conv.stop_token_ids:
+                    stop_token_ids_index = [i for i, id in enumerate(output_ids) if id in conv.stop_token_ids]
+                    if len(stop_token_ids_index) > 0:
+                        output_ids = output_ids[: stop_token_ids_index[0]]
+
+                output = tokenizer.decode(output_ids, spaces_between_special_tokens=False,)
+                if conv.stop_str and output.find(conv.stop_str) > 0:
+                    output = output[: output.find(conv.stop_str)]
+                for special_token in tokenizer.special_tokens_map.values():
+                    if isinstance(special_token, list):
+                        for special_tok in special_token:
+                            output = output.replace(special_tok, "")
+                    else:
+                        output = output.replace(special_token, "")
+                output = output.strip()
+
+                if conv.name == "xgen" and output.startswith("Assistant:"):
+                    output = output.replace("Assistant:", "", 1).strip()
+
+                turns.append(output)
+                idxs.append(int(idx))
+                new_tokens.append(int(new_token))
+                wall_time.append(total_time)
+                conv.messages[-1][-1] = output
+            # torch.cuda.empty_cache()
+            choices.append({"index": i, 
+                            "turns": turns, 
+                            "idxs": idxs, 
+                            "new_tokens": new_tokens, 
+                            "wall_time": wall_time
+                            })
+
+        # Dump answers
+        os.makedirs(os.path.dirname(answer_file), exist_ok=True)
+        with open(os.path.expanduser(answer_file), "a") as fout:
+            ans_json = {
+                "question_id": question["question_id"],
+                "answer_id": shortuuid.uuid(),
+                "model_id": model_id,
+                "choices": choices,
+                "tstamp": time.time(),
+            }
+            fout.write(json.dumps(ans_json) + "\n")
+
+
+def reorg_answer_file(answer_file):
+    """Sort by question id and de-duplication"""
+    answers = {}
+    with open(answer_file, "r") as fin:
+        for l in fin:
+            qid = json.loads(l)["question_id"]
+            answers[qid] = l
+
+    qids = sorted(list(answers.keys()))
+    with open(answer_file, "w") as fout:
+        for qid in qids:
+            fout.write(answers[qid])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ea_model_path",type=str,default="/home/haiduo/code/Jakiro/checkpoints/Jakiro-Vicuna-7B-v1.3", help="The path to the weights.",)
+    parser.add_argument("--base_model_path", type=str, default="/home/haiduo/model/vicuna/vicuna-7b-v1.3", help="1")
+    parser.add_argument("--load_in_8bit", default=False, action="store_false", help="Use 8-bit quantization")
+    parser.add_argument("--model_id", type=str, default="ess-vicuna-7b-fp16")
+    parser.add_argument("--bench_name", type=str, default="spec_bench", help="The name of the benchmark question set.",)
+    parser.add_argument("--question_begin", type=int, help="A debug option. The begin index of questions.",)
+    parser.add_argument("--question_end", type=int, help="A debug option. The end index of questions.")
+    parser.add_argument("--answer_file", type=str,default="/home/haiduo/code/Spec-Bench/outputs/jakiro/A40", help="The output path of answer file.")
+    parser.add_argument("--total_token", type=int, default=60, choices=[60,50,48], help="Maximum number of new tokens generated during the draft process",)
+    parser.add_argument("--depth", type=int, default=5, help="Maximum depth of the tree, which is the maximum length of the draft model's output each time",)
+    parser.add_argument("--top_k", type=int, default=10, help="Candidate tokens tree selected at each layer during tree construction.",)
+    parser.add_argument("--num_choices", type=int, default=1, help="How many completion choices to generate.",)
+    parser.add_argument("--num_gpus_per_model", type=int, default=1, help="The number of GPUs per model.",)
+    parser.add_argument("--num_gpus_total", type=int, default=1, help="The total number of GPUs.")
+    parser.add_argument("--temperature", type=float, default=0.0,)
+    parser.add_argument("--tree-choices", type=str, default="mc_sim_7b_63",)
+
+    args = parser.parse_args()
+
+    args.model_id = args.model_id + "-temperature-" + str(args.temperature)
+    if args.num_gpus_total // args.num_gpus_per_model > 1:
+        import ray
+        ray.init()
+
+    question_file = f"{parent_dir}/data/{args.bench_name}/question.jsonl"
+    answer_file = f"{args.answer_file}/{args.bench_name}/{args.model_id}.jsonl"
+    print(f"Output to {answer_file}")
+
+    run_eval(
+        args.base_model_path,
+        args.ea_model_path,
+        args.model_id,
+        question_file,
+        args.question_begin,
+        args.question_end,
+        answer_file,
+        args.num_choices,
+        args.num_gpus_per_model,
+        args.num_gpus_total,
+        args.temperature,
+        args
+    )
+
+    reorg_answer_file(answer_file)
